589302594b54a1072672eac969b4ff515ff80ada
SG0303262 <rajesh.gangisetty.ctr@sabre.com>
2020-04-03 18:59:08 +0530

    my first project

---
+++ BankInsurance_Xom/src/com/Qualorbis/InputEntry.java
@@ -0,0 +1,17 @@
+package com.Qualorbis;
+
+import java.io.Serializable;
+
+public class InputEntry implements Serializable {
+
+	private int year;
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
---
+++ BankInsurance_Xom/src/com/Qualorbis/OutputEntry.java
@@ -0,0 +1,20 @@
+package com.Qualorbis;
+
+import java.io.Serializable;
+
+public class OutputEntry implements Serializable{
+	
+	private int interestRate;
+
+	public int getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(int interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	
+	
+
+}
